db9094a7969e6459074cdc76a20b8f3126c98919
sassierl <149876150+sassierl@users.noreply.github.com>
2026-08-20 23:37:56 +0200

    Added phase_deal and game struct

--- game/game.go
+++ game/game.go
@@ -1,1 +1,21 @@
 package game
+
+type Game struct {
+	Players []Player
+	Deck    Deck
+	State   GameState
+}
+
+func NewGame(players []Player) *Game {
+	return &Game{
+		Players: players,
+		Deck:    NewDeck(),
+		State: &GameState{
+			Phase:       PhaseDeal,
+			CurrentTurn: 0,
+			Taker:       nil,
+			Bid:         0,
+			Round:       1,
+		},
+	}
+}
--- game/state.go
+++ game/state.go
@@ -1,1 +1,30 @@
 package game
+
+type Phase int
+
+const (
+	PhaseDeal Phase = iota
+	PhaseAuction
+	PhasePlay
+	PhaseScoring
+)
+
+type GameState struct {
+	Phase       Phase
+	CurrentTurn int
+	Taker       *Player
+	Bid         int
+	Round       int
+}
+
+func (gs *GameState) NextPhase() {
+	gs.Phase++
+	if gs.Phase > PhaseScoring {
+		gs.Phase = PhaseDeal
+	}
+}
+
+func (gs *GameState) SetTaker(player *Player, bid int) {
+	gs.Taker = player
+	gs.Bid = bid
+}
